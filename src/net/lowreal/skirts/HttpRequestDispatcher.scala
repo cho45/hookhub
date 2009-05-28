281ffee9f1e0ec90b4f4f46f6440cd3ddf10a70a
cho45 <cho45@lowreal.net>
2009-05-29 03:36:44 +0900

    static をハンドル

--- src/net/lowreal/skirts/HttpRequestDispatcher.scala
+++ src/net/lowreal/skirts/HttpRequestDispatcher.scala
@@ -12,29 +12,37 @@ class HttpRequestDispatcher extends Filter {
 		println("doFilter")
 		(request, response) match {
 			case (req: HttpServletRequest, res: HttpServletResponse) =>
+				println("")
 				println(req)
-				println(res)
-				res.setContentType("text/html")
-				res.getWriter.println { """<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">""" }
-				res.getWriter.println {
-					<html>
-						<head><title>Hello World</title></head>
-						<style type='text/css'><![CDATA[
-							pre {
-								background: #efefef;
-								border: #ccc;
-								padding: 0.5em;
-							}
-						]]></style>
-					<body>
-						<h1>Request</h1>
-						<pre>Method: { req.getMethod }</pre>
-						<pre>RequestURI: { req.getRequestURI }</pre>
-						<pre>QueryString: { req.getQueryString }</pre>
-						<h1>Raw</h1>
-						<pre>{ req }</pre>
-					</body>
-					</html>
+				val static = """^/(?:css|js|images|static).*""".r
+				val path   = req.getRequestURI().toString()
+				println(path)
+				println(static.unapplySeq(path))
+				println(static.findFirstIn(path))
+				path match {
+					case static() =>
+						println("static")
+						chain.doFilter(request, response)
+					case _ =>
+						println("404")
+						res.setContentType("text/html")
+						res.getWriter.println { """<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">""" }
+						res.getWriter.println {
+							<html>
+								<head><title>Hello World</title></head>
+
+								<link rel="stylesheet" type="text/css" href="/css/base.css" media="screen,tv"/>
+								<!-- script type="text/javascript" src="/js/site-script.js"></script -->
+							<body>
+								<h1>Request</h1>
+								<pre>Method: { req.getMethod }</pre>
+								<pre>RequestURI: { req.getRequestURI }</pre>
+								<pre>QueryString: { req.getQueryString }</pre>
+								<h1>Raw</h1>
+								<pre>{ req }</pre>
+							</body>
+							</html>
+						}
 				}
 			case _ => chain.doFilter(request, response)
 		}
