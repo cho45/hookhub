e4f78d29e648168ff3a635759e879912f9667fe2
cho45 <cho45@lowreal.net>
2009-06-04 03:41:49 +0900

    定義を整理

--- src/net/lowreal/hookhub/Application.scala
+++ src/net/lowreal/hookhub/Application.scala
@@ -3,23 +3,6 @@ package net.lowreal.hookhub
 import net.lowreal.skirts._
 import javax.servlet.http.{HttpServlet, HttpServletResponse, HttpServletRequest}
 
-trait Req {
-	def method = ""
-
-	def unapply (req: HttpServletRequest) = {
-		if ( method == "" || req.getMethod == method ) {
-			Some(req.getRequestURI)
-		} else {
-			Some()
-		}
-	}
-}
-
-object GET  extends Req { override def method = "GET" }
-object POST extends Req { override def method = "POST" }
-object PUT  extends Req { override def method = "PUT" }
-object HEAD extends Req { override def method = "HEAD" }
-
 class AppHttpRouter extends HttpRouter {
 
 	override def dispatch (req: HttpServletRequest, res: HttpServletResponse):Unit = req match {
@@ -33,6 +16,24 @@ class AppHttpRouter extends HttpRouter {
 
 		case _ => {
 			println("404")
+			res.setContentType("text/html")
+			res.getWriter.println { """<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">""" }
+			res.getWriter.println {
+				<html>
+					<head><title>404</title></head>
+
+					<link rel="stylesheet" type="text/css" href="/css/base.css" media="screen,tv"/>
+					<!-- script type="text/javascript" src="/js/site-script.js"></script -->
+				<body>
+					<h1>Request</h1>
+					<pre>Method: { req.getMethod }</pre>
+					<pre>RequestURI: { req.getRequestURI }</pre>
+					<pre>QueryString: { req.getQueryString }</pre>
+					<h1>Raw</h1>
+					<pre>{ req }</pre>
+				</body>
+				</html>
+			}
 		}
 	}
 }
