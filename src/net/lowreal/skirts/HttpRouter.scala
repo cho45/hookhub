fa10f4ec8cabea118d527ef3bdfd0e4822eab606
cho45 <cho45@lowreal.net>
2009-06-04 02:34:57 +0900

    相互依存を解消

---
+++ src/net/lowreal/skirts/HttpRouter.scala
@@ -0,0 +1,30 @@
+package net.lowreal.skirts
+
+import javax.servlet._
+import javax.servlet.http.{HttpServlet, HttpServletResponse, HttpServletRequest}
+
+class HttpRouter {
+	def dispatch (req: HttpServletRequest, res: HttpServletResponse):Unit = {
+		print("router!")
+
+		res.setContentType("text/html")
+		res.getWriter.println { """<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">""" }
+		res.getWriter.println {
+			<html>
+				<head><title>Hello World</title></head>
+
+				<link rel="stylesheet" type="text/css" href="/css/base.css" media="screen,tv"/>
+				<!-- script type="text/javascript" src="/js/site-script.js"></script -->
+			<body>
+				<h1>Request</h1>
+				<pre>Method: { req.getMethod }</pre>
+				<pre>RequestURI: { req.getRequestURI }</pre>
+				<pre>QueryString: { req.getQueryString }</pre>
+				<h1>Raw</h1>
+				<pre>{ req }</pre>
+			</body>
+			</html>
+		}
+	}
+}
+
