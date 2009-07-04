6fe88b01abefdaf56852035a5dd73e428ac09b1b
cho45 <cho45@lowreal.net>
2009-07-04 23:36:48 +0900

    include

--- src/net/View.scala
+++ src/net/View.scala
@@ -4,22 +4,27 @@ package net.lowreal.hookhub
 import org.mozilla.javascript._
 class RhinoView[T <: net.lowreal.skirts.Context] {
 	def apply(name:String, context:T) = {
+		context.res.header("Content-Type", "text/html")
+		context.res.content(render(name, context))
+	}
+
+	def render (name:String, context:T):String = {
 		val ctx = Context.enter()
 		ctx.setLanguageVersion(Context.VERSION_1_7)
 
 		val scope = ctx.initStandardObjects()
-		val jsskirtcontext = Context.javaToJS(context, scope);
-		ScriptableObject.putProperty(scope, "c", jsskirtcontext);
+
+		ScriptableObject.putProperty(scope, "c", Context.javaToJS(context, scope));
+		ScriptableObject.putProperty(scope, "v", Context.javaToJS(this, scope));
 
 		val template = context.file(name + ".html")
 		ScriptableObject.putProperty(scope, "template", Context.javaToJS(template, scope));
 
+
 		val ejs = context.file("js/ejs.js")
 		val result = ctx.evaluateString(scope, ejs, name, 1, null) 
 
-		val ret = Context.toString(result)
-		context.res.header("Content-Type", "text/html")
-		context.res.content(ret)
+		Context.toString(result)
 	}
 }
 
