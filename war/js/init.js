29506d3c3549f173fc4356514b2b41be2d2437b2
cho45 <cho45@lowreal.net>
2009-07-08 02:20:35 +0900

    Document

--- war/js/init.js
+++ war/js/init.js
@@ -11,16 +11,32 @@ Global = (function () {
 		})
 	};
 
-	http.post  = function (url, data) {
+	http.post  = function (url, params) {
 		return http({
 			method  : "post",
 			url     : url,
-			data    : data,
+			data    : http.data(params),
 			headers : {
 				"Content-Type":"application/x-www-form-urlencoded"
 			}
 		})
 	};
+
+	http.data = function (params) {
+		var ret = [];
+		for (var key in params) if (params.hasOwnProperty(key)) {
+			var val = params[key];
+			if (typeof(val) == "string") {
+				ret.push(encodeURIComponent(key) + "=" + encodeURIComponent(val));
+			} else
+			if (val instanceof Array) {
+				for (var i = 0, len = val.length; i < len; i++) {
+					ret.push(encodeURIComponent(key) + "=" + encodeURIComponent(val[i]));
+				}
+			}
+		}
+		return ret.join('&');
+	}
 
 	delete this._http;
 	delete stash.params.token;
